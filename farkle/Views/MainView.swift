76a58fc3978d4004eef6588c311c7ac0e1759f33
Joe Lucero <>
2019-12-18 07:00:00 -0700

    Renaming of files

--- farkle/Views/MainView.swift
+++ farkle/Views/MainView.swift
@@ -6,23 +6,39 @@
 //  Copyright © 2019 Joe Lucero. All rights reserved.
 //
 
-//import SwiftUI
-//
-//struct MainView: View {
-//    @State var numberOfOpponents = 5
-//
-//    var body: some View {
-//        GeometryReader { geo in
-//            DiceRollingAreaView(dice: DiceManager().dice)
-//            .frame(width: 0.6 * geo.frame(in: .local).width,
-//                   height: geo.frame(in: .local).width,
-//                   alignment: .center)
-//        }
-//    }
-//}
-//
-//struct MainView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MainView()
-//    }
-//}
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                VStack {
+                    Spacer()
+                    ForEach(GameManager.leftOpponents) { opponent in
+                        OpponentView(opponent: opponent, isOpponentOnLeft: true)
+                        Spacer()
+                        Spacer()
+                    }
+                }
+
+                DiceRollingAreaView(dice: DiceManager().dice)
+                VStack {
+                    Spacer()
+                    ForEach(GameManager.rightOpponents) { opponent in
+                        OpponentView(opponent: opponent, isOpponentOnLeft: false)
+                        Spacer()
+                        Spacer()
+                    }
+                }
+            }
+            PlayerView(player: GameManager.player)
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
